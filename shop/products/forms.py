862bfb3fd492334fb4ffac91885a12ba8a09052e
babaorere <baba.orere@gmail.com>
2020-10-21 12:59:04 -0300

    Programa Original

---
+++ shop/products/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import Form, IntegerField, FloatField, StringField, TextAreaField, validators
+
+class Addproducts(Form):
+    name = StringField('Name', [validators.DataRequired()])
+    price = FloatField('Price', [validators.DataRequired()])
+    discount = IntegerField('Discount', default=0)
+    stock = IntegerField('Stock', [validators.DataRequired()])
+    colors = StringField('Colors', [validators.DataRequired()])
+    discription = TextAreaField('Discription', [validators.DataRequired()])
+
+    image_1 = FileField('Image 1',
+                        validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'Images only please'])
+    image_2 = FileField('Image 2',
+                        validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'Images only please'])
+    image_3 = FileField('Image 3',
+                        validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'Images only please'])
